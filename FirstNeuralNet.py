5f9738e04b4e400b249d39f97852559f5fbbcb59
Gustavo Souza <gustavo.souza1156.gg@gmail.com>
2018-08-01 05:20:35 -0300

    Update FirstNeuralNet.py

--- FirstNeuralNet.py
+++ FirstNeuralNet.py
@@ -9,7 +9,7 @@ class NeuralNetwork():
         # We model a single neuron, with 3 input connections and 1 output connection.
         # We assign np.random weights to a 3 x 1 matrix, with values in the range -1 to 1
         # and mean 0.
-        self.synaptic_weights = 2 * np.random.np.random((3, 1)) - 1
+        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
 
     # The Sigmoid function, which describes an S shaped curve.
     # We pass the weighted sum of the inputs through this function to
